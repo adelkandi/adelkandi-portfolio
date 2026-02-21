27b8f108d4cde22c2c1be0ae5185d1e00179fcd0
Adel Kandi <83815382+adelkandi@users.noreply.github.com>
2026-02-21 15:02:29 -0500

    feat: add Hero component with Navbar and introductory text

---
+++ adelkandi-portfolio/src/pages/Hero.tsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar"
+
+export default function Hero(){
+    return(
+        <>
+            <Navbar />
+            <div className="container">
+                <div className="presntation-container text-6xl">
+                    <h1>Hi,</h1><h2> I'm Adel</h2>
+                </div>
+                <img src=""/>
+            </div>
+        </>
+    )
+}
