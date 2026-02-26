7c8cb853b52660d99d90d0f2bd6ee8bed18b401d
Adel Kandi <83815382+adelkandi@users.noreply.github.com>
2026-02-26 12:06:46 -0500

    feat: add Home component with Navbar, Hero, and About sections

---
+++ adelkandi-portfolio/src/pages/Home.tsx
@@ -0,0 +1,16 @@
+// Import Sections
+import Hero from "../sections/Hero";
+import Navbar from "../components/Navbar";
+import About from "../sections/About";
+
+export default function Home(){
+    return(
+        <>
+            <Navbar />
+            <Hero />
+            <About />
+        </>
+    )
+
+
+}
