4ac0c4d8cc52512e4dc89a695fb5990b91de38aa
Adel Kandi <83815382+adelkandi@users.noreply.github.com>
2026-03-04 03:40:16 -0500

    feat: restructure Home component to include all sections and improve layout

--- adelkandi-portfolio/src/pages/Home.tsx
+++ adelkandi-portfolio/src/pages/Home.tsx
@@ -3,14 +3,26 @@ import Hero from "../sections/Hero";
 import Navbar from "../components/Navbar";
 import About from "../sections/About";
 import Skills from "../sections/Skills";
+import Projects from "../sections/Projects";
+import Contact from "../sections/Contact";
+import Footer from "../components/Footer";
 
 export default function Home(){
     return(
         <>
+            {/* Navbar */}
             <Navbar />
+
+            {/* Sections */}
             <Hero />
             <About />
             <Skills />
+            <Projects />
+            <Contact />
+            {/* Footer */}
+            <Footer />
+
+
         </>
     )
 
