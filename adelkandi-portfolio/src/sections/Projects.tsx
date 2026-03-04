820370f7d0b5beef556154bc0ccb654db4b761f8
Adel Kandi <83815382+adelkandi@users.noreply.github.com>
2026-03-04 03:42:39 -0500

    feat: enhance Projects section with initial layout and content

--- adelkandi-portfolio/src/sections/Projects.tsx
+++ adelkandi-portfolio/src/sections/Projects.tsx
@@ -1,11 +1,27 @@
 
 
 
-export default function Projects(){
-    // Variables 
+export default function Projects(){ 
 
     return(
         <>
+            <section id="projects" className="skills-section  px-[8rem]">
+            <div className="">
+                <div className="mb-12">
+                    <div className="flex items-center gap-4 mb-16">
+                        <div className="h-1 w-12 bg-[#f6b452]"></div>
+                        <h1 className="text-4xl sm:text-5xl font-bold text-[#F8F8FF]">Projects</h1>
+                    </div>
+                </div>
+                {/* Projects Cards */}
+                <div className="flex flex-col items-center justify-center py-16">
+                    <div className="text-5xl text-[#f6b452] mb-4">🚀</div>
+                    <div className="text-4xl font-semibold text-[#f6b452] mb-2">Coming Soon</div>
+                    <p className="text-[#F8F8FF] text-lg">Exciting projects coming your way...</p>
+                </div>
+
+            </div>
+            </section>
         </>
     )
 }
