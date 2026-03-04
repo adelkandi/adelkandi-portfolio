70c0a1fa32bb2681aa6ed5f09a9c615f91f16758
Adel Kandi <83815382+adelkandi@users.noreply.github.com>
2026-03-03 21:03:12 -0500

    feat: update Navbar links to use anchor tags and add About, Skills, Projects, and Contact sections feat: create About, Contact, Hero, and Projects sections with initial content

--- adelkandi-portfolio/src/components/Navbar.tsx
+++ adelkandi-portfolio/src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Logo from './Logo.tsx';
 
 function Navbar(){
     return(
-        <nav className="bgw-full bg-transparent">
+        <nav className="w-full bg-transparent sticky top-0 z-50">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">  
                 <div className="flex items-center justify-between h-20">
                     {/* Logo */}
@@ -12,11 +12,11 @@ function Navbar(){
 
                     {/* Links - centered */}
                     <div className="hidden md:flex items-center justify-center flex-1 space-x-8">
-                        <a href="/" className="text-[#F8F8FF] hover:text-[#5ad1b2] transition-colors">Home</a>
-                        <a href="/about" className="text-[#F8F8FF] hover:text-[#5ad1b2] transition-colors">About</a>
-                        <a href="/projects" className="text-[#F8F8FF] hover:text-[#5ad1b2] transition-colors">Skills</a>
-                        <a href="/projects" className="text-[#F8F8FF] hover:text-[#5ad1b2] transition-colors">Projects</a>
-                        <a href="/contact" className="text-[#F8F8FF] hover:text-[#5ad1b2] transition-colors">Contact Me</a>
+                        <a href="#" className="text-[#F8F8FF] hover:text-[#5ad1b2] transition-colors">Home</a>
+                        <a href="#about" className="text-[#F8F8FF] hover:text-[#5ad1b2] transition-colors">About</a>
+                        <a href="#skills" className="text-[#F8F8FF] hover:text-[#5ad1b2] transition-colors">Skills</a>
+                        <a href="#projects" className="text-[#F8F8FF] hover:text-[#5ad1b2] transition-colors">Projects</a>
+                        <a href="#contact" className="text-[#F8F8FF] hover:text-[#5ad1b2] transition-colors">Contact Me</a>
                     </div>
 
                     
