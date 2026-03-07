0a4d4be1d209af844dcf9f1b7021d36005379202
Adel Kandi <83815382+adelkandi@users.noreply.github.com>
2026-03-07 15:32:00 -0500

    fix: update XTwitter link in Footer component

--- adelkandi-portfolio/src/components/Footer.tsx
+++ adelkandi-portfolio/src/components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer(){
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <a href="https://github.com/adelkandi" target="_blank" className="opacity-70 m-5 inline-flex items-center gap-2 hover:opacity-100"><FaGithub size={20} /> Github</a>
                     <a href="https://www.linkedin.com/in/adel-kandi-416bb925b/" target="_blank" className="opacity-70 m-5 inline-flex items-center gap-2 hover:opacity-100"><FaLinkedin size={20} /> LinkedIn</a>
-                    <a href="https://x.com" target="_blank" className="opacity-70 inline-flex items-center gap-2 hover:opacity-100"><FaXTwitter size={20} /> XTwitter</a>
+                    <a href="https://x.com/adel_kandi" target="_blank" className="opacity-70 inline-flex items-center gap-2 hover:opacity-100"><FaXTwitter size={20} /> XTwitter</a>
                 </div>
                 
 
