08b8b94f759d3838335722da0d4620911cd4ff26
Adel Kandi <83815382+adelkandi@users.noreply.github.com>
2026-03-07 06:01:01 -0500

    feat: integrate emailjs for contact form functionality and update section styling

--- adelkandi-portfolio/src/sections/Contact.tsx
+++ adelkandi-portfolio/src/sections/Contact.tsx
@@ -1,22 +1,21 @@
+import ContactForm from "../components/ContactForm"
+import emailjs from '@emailjs/browser'
+
 
 
 export default function Contact(){
     return(
         <>
+        {/* Contact Section */}
             <section id="contact" className="skills-section  px-[8rem]">
             <div className="">
                 <div className="mb-12">
                     <div className="flex items-center gap-4 mb-8">
-                        <div className="h-1 w-12 bg-[#f6b452]"></div>
+                        <div className="h-1 w-12 bg-[#5ad1b2]"></div>
                         <h1 className="text-4xl sm:text-5xl font-bold text-[#F8F8FF]">Contact Me</h1>
                     </div>
                 </div>
-                {/* Contact Form */}
-                <div className="flex flex-col items-center justify-center py-16">
-                    <div className="text-5xl text-[#f6b452] mb-4">📬</div>
-                    <div className="text-4xl font-semibold text-[#f6b452] mb-2">Coming Soon</div>
-                    <p className="text-[#F8F8FF] text-lg">Let's get in touch soon...</p>
-                </div>
+                <ContactForm />
 
             </div>
             </section>
