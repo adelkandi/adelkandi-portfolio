bfef858c68d811048f48a323a2c476b03cf1fbbe
Adel Kandi <83815382+adelkandi@users.noreply.github.com>
2026-03-10 01:10:18 -0400

    fix: enhance Logo component styling with hover effects

--- adelkandi-portfolio/src/components/Logo.tsx
+++ adelkandi-portfolio/src/components/Logo.tsx
@@ -11,8 +11,8 @@ const styles = {
 
 export default function Logo(){
     return (
-        <div style={styles.logo} className="w-auto cursor-pointer ">
-           {"<"}Adel {"/>"}
+        <div style={styles.logo} className="w-auto cursor-pointer transition-transform duration 300 hover:scale-105 hover:text-[#5ad1b2]">
+           {"<"} Adel {"/>"}
         </div>
     )
 }
