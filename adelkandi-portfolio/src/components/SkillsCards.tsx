3b54b08e75561be0aae0c8e07c6c4a3d6c5d28d6
Adel Kandi <83815382+adelkandi@users.noreply.github.com>
2026-03-15 17:49:01 -0400

    fix: update SkillsCard component layout for improved responsiveness

--- adelkandi-portfolio/src/components/SkillsCards.tsx
+++ adelkandi-portfolio/src/components/SkillsCards.tsx
@@ -6,7 +6,7 @@ interface SkillsCardProps{
 
 const SkillsCard = ({category}: SkillsCardProps) =>{
     return(
-        <div className="w-[500px] h-[250px] border border-[#f6b452] rounded-2xl shadow-lg p-6 bg-[#1E1E1E] transition duration-300 ease-in-out hover:scale-105 hover:shadow-[#f6b452]">
+        <div className="h-auto w-full md:w-[500px] md:h-[250px] lg:w-[500px] lg:h-[250px] flex flex-col  border border-[#f6b452] rounded-2xl shadow-lg p-6 bg-[#1E1E1E] transition duration-300 ease-in-out hover:scale-105 hover:shadow-[#f6b452] items-stretch">
             <h3 className="text-3xl text-[#f6b452] p-2">{category.titleLogo} {category.title}</h3>
             <ul className="p-2 space-y-2">
                 {category.skills.map((skill, index) => (<li key={index} className="flex items-center gap-2"><span className="text-[#f6b452]">{skill.icon}</span>{skill.name}</li>))}
