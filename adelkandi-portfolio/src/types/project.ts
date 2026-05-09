82e4adee73a4fd4fcdf6e36091c219ce2b6cd91f
Adel Kandi <83815382+adelkandi@users.noreply.github.com>
2026-05-09 12:59:26 -0400

    feat: add Project and Details interfaces for project structure

---
+++ adelkandi-portfolio/src/types/project.ts
@@ -0,0 +1,12 @@
+// Project Variables.
+import type {ReactNode} from 'react';
+
+// Project Header:
+
+export interface Project{
+    title: string;
+    titleLogo: ReactNode;
+}
+export interface Details{
+    
+}
