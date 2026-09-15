f619105cc9872bb2ba995f6a122418d138eaa1b8
Adel Kandi <83815382+adelkandi@users.noreply.github.com>
2026-09-14 20:00:12 -0400

    refactor: enhance Projects component layout and structure for better readability

--- adelkandi-portfolio/src/sections/Projects.tsx
+++ adelkandi-portfolio/src/sections/Projects.tsx
@@ -1,20 +1,24 @@
 
+import ProjectCard from "../components/ProjectCard";
+import {projectsData} from "../data/projects";
 
-
-export default function Projects(){ 
+export default function Projects(){
 
     return(
         <>
-            <section id="projects" className="skills-section px-8 sm:px-6  lg:px-[8rem]">
+            <section id="projects" className="skills-section mb-10 px-8 sm:px-6 lg:px-[8rem]">
             <div className="">
-                <div className="mb-12">
-                    <div className="flex items-center gap-4 mb-16">
+                    <div className="mb-12">
+                        <div className="mb-8 flex items-center gap-4">
                         <div className="h-1 w-12 bg-[#f6b452]"></div>
                         <h1 className="text-4xl sm:text-5xl font-bold text-[#F8F8FF]">Projects</h1>
                     </div>
                 </div>
-                {/* Projects Cards */}
-
+                <div className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3">
+                    {projectsData.map((project) => (
+                        <ProjectCard key={project.title} project={project}/>
+                    ))}
+                </div>
             </div>
             </section>
         </>
